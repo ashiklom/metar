ee36d418fceba539045ec3ad53b0a8bd2d750d7a
Alexey Shiklomanov <ashiklom@bu.edu>
2018-03-16 15:47:37 -0400

    Make `list2df` more robust, and add test

--- R/list2df.R
+++ R/list2df.R
@@ -4,10 +4,21 @@
 #' @return `tbl` form of the list, with list columns where appropriate
 #' @export
 list2df <- function(l) {
-  purrr::map(l, make_list_cols) %>%
+  list_cols <- purrr::map(l, find_list_cols) %>%
+    purrr::reduce(union)
+  purrr::map(l, set_list_cols, list_cols) %>%
     dplyr::bind_rows()
 }
 
-make_list_cols <- function(l) {
-  purrr::map_if(l, is.list, ~list(.))
+find_list_cols <- function(l) {
+  lgl <- purrr::map_lgl(l, ~length(.) > 1 || is.list(.))
+  out <- names(which(lgl))
+  if (is.null(out) || any(is.na(out))) {
+    stop("All list columns must be named, because order can be ambiguous.")
+  }
+  out
+}
+
+set_list_cols <- function(l, inds) {
+  purrr::map_at(l, inds, ~list(.))
 }
--- tests/testthat/test.yaml_header.R
+++ tests/testthat/test.yaml_header.R
@@ -23,3 +23,23 @@ test_that(
     expect_equal(attr(yaml_test, "nlines"), 27)
   }
 )
+
+test_that(
+  "YAML list gets correctly converted to a data.frame",
+  {
+    test_data <- list(
+      list(a = 3, b = 6, c = 9, d = 1:5),
+      list(a = 4, b = 7, c = 10, d = 1:10),
+      list(a = 5, c = 11:15, d = 8, f = 12)
+    )
+    tmp <- tempfile()
+    write_yaml(test_data, tmp)
+    yaml_test <- read_yaml_header(tmp)
+    df <- list2df(yaml_test)
+    expect_equal(df$a, 3:5)
+    expect_equal(df$b, c(6, 7, NA))
+    expect_equal(df$c, list(9, 10, 11:15))
+    expect_equal(df$d, list(1:5, 1:10, 8))
+    expect_equal(df$f, c(NA, NA, 12))
+  }
+)
